c7417f101cd7d358069babedb7583fba4049e703
jack mott <jack.mott@gmail.com>
2018-01-04 20:54:35 -0600

    ep2

---
+++ basics/basics.go
@@ -0,0 +1,66 @@
+package main
+
+func main() {
+
+	/*
+		var a int8  // -127,127
+		var b uint8 // 0  255
+		var c int16
+		var d uint16
+		var e int // 32bits on 32bit machiens  64bits on 64bit machines
+
+		//3.14159
+		var f1 float32
+		var f2 float64
+	*/
+
+	// Math is not real!
+	/*
+		a := 2.0
+		b := 3.0
+		r := a / b
+		fmt.Printf("%.20f\n", r)
+	*/
+
+	/* booleans
+	var b bool = true
+	fmt.Println(torf)
+	*/
+
+	/* strings
+	helloWorld := "Hello World"
+	fmt.Println(helloWorld)
+	*/
+
+	// Infinite Loop
+	/*	for {
+			fmt.Println("Hello World")
+		}
+	*/
+
+	// While
+	/*
+		i := 0
+		for i < 10 {
+			fmt.Println("Hello World:", i)
+			i = i + 1
+		}*/
+
+	// For
+	/*
+		for i := 0; i < 10; i++ {
+			fmt.Println("Hello World:", i)
+		}*/
+
+	// Conditionals
+	/*
+		x := 1
+		if x > 5 {
+			fmt.Println("Hello World")
+		} else if x < 5 {
+			fmt.Println("Goodbye")
+		} else {
+			fmt.Println("IT IS FIVE!")
+		}*/
+
+}
---
+++ guess/guess.go
@@ -0,0 +1,47 @@
+package main
+
+// 1.  Make the program print out how many tries it took to win.
+// 2.  See if you can tell if the user is lying
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+
+	scanner := bufio.NewScanner(os.Stdin)
+
+	low := 1
+	high := 100
+
+	fmt.Println("Please think of a number between", low, "and", high)
+	fmt.Println("Press ENTER when ready")
+	scanner.Scan()
+
+	for {
+		// Binary Search Strategy
+		guess := (low + high) / 2
+		fmt.Println("I guess the number is", guess)
+		fmt.Println("Is that:")
+		fmt.Println("(a) too high?")
+		fmt.Println("(b) too low?")
+		fmt.Println("(c) correct?")
+		scanner.Scan()
+		response := scanner.Text()
+
+		if response == "a" {
+			high = guess - 1
+		} else if response == "b" {
+			low = guess + 1
+		} else if response == "c" {
+			fmt.Println("I won!")
+			break
+		} else {
+			fmt.Println("Invalid response, try again.")
+		}
+
+	}
+
+}
