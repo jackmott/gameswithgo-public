2c4e6a7dcbd7b0b14bb6fcd1b3fe20a26f2cf2d1
jack mott <jack.mott@gmail.com>
2018-01-13 20:54:12 -0600

    episode 6, sdl2

---
+++ sdl2/sdl2.go
@@ -0,0 +1,66 @@
+package main
+
+// Experiment! draw some crazy stuff!
+// Gist it next week and I'll show it off on stream
+
+import (
+	"fmt"
+
+	"github.com/veandco/go-sdl2/sdl"
+)
+
+const winWidth, winHeight int = 800, 600
+
+type color struct {
+	r, g, b byte
+}
+
+func setPixel(x, y int, c color, pixels []byte) {
+	index := (y*winWidth + x) * 4
+
+	if index < len(pixels)-4 && index >= 0 {
+		pixels[index] = c.r
+		pixels[index+1] = c.g
+		pixels[index+2] = c.b
+	}
+
+}
+
+func main() {
+
+	window, err := sdl.CreateWindow("Testing SDL2", sdl.WINDOWPOS_UNDEFINED, sdl.WINDOWPOS_UNDEFINED,
+		int32(winWidth), int32(winHeight), sdl.WINDOW_SHOWN)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer window.Destroy()
+
+	renderer, err := sdl.CreateRenderer(window, -1, sdl.RENDERER_ACCELERATED)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer renderer.Destroy()
+
+	tex, err := renderer.CreateTexture(sdl.PIXELFORMAT_ABGR8888, sdl.TEXTUREACCESS_STREAMING, int32(winWidth), int32(winHeight))
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer tex.Destroy()
+
+	pixels := make([]byte, winWidth*winHeight*4)
+
+	for y := 0; y < winHeight; y++ {
+		for x := 0; x < winWidth; x++ {
+			setPixel(x, y, color{byte(x % 255), byte(y % 255), 0}, pixels)
+		}
+	}
+
+	tex.Update(nil, pixels, winWidth*4)
+	renderer.Copy(tex, nil, nil)
+	renderer.Present()
+	sdl.Delay(2000)
+
+}
