0b5c9982c84dce5a5724e02fe9b648e2dc8ac5ab
jack mott <jack.mott@gmail.com>
2018-01-22 13:23:10 -0600

    fix for pong timing error

--- pong/pong.go
+++ pong/pong.go
@@ -299,7 +299,7 @@ func main() {
 
 		elapsedTime = float32(time.Since(frameStart).Seconds())
 		if elapsedTime < .005 {
-			sdl.Delay(5 - uint32(elapsedTime/1000.0))
+			sdl.Delay(5 - uint32(elapsedTime*1000.0))
 			elapsedTime = float32(time.Since(frameStart).Seconds())
 		}
 	}
